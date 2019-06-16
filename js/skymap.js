e00f11ee6208a241b7dbc89f2cae8306d07bd6b9
Kousuke Kawahira <kawahira@binzume.net>
2019-06-16 13:59:51 +0900

    rename component

--- js/skymap.js
+++ js/skymap.js
@@ -11,7 +11,7 @@ function instantiate(id, parent) {
 	return el;
 }
 
-AFRAME.registerComponent('stars', {
+AFRAME.registerComponent('celestial-sphere', {
 	schema: {
 		src: { type: 'string', default: "hip_stars.json" },
 		lat: { type: 'number', default: 35 },
@@ -22,6 +22,7 @@ AFRAME.registerComponent('stars', {
 		speed: { type: 'number', default: 1 },
 		magFactor: { type: 'number', default: 0.6 },
 		magOffset: { type: 'number', default: -0.5 },
+		radius: { type: 'number', default: 4000 },
 		constellationSrc: { type: 'string', default: "" },
 		constellation: { type: 'boolean', default: false },
 		debug: { type: 'boolean', default: true }
@@ -79,7 +80,7 @@ void main() {
 		starMaterial.uniforms.size.value = 3;
 		this.starMaterial = starMaterial;
 		if (this.data.realtime) {
-			this.el.setAttribute("stars", { timeMs: Date.now() });
+			this.el.setAttribute("celestial-sphere", { timeMs: Date.now() });
 		}
 		this.currentSpeed = 0;
 		this.gridLastUpdated = 0;
@@ -93,7 +94,7 @@ void main() {
 			for (let i = 0; i < result.length; i++) {
 				var star = result[i];
 
-				let v = new THREE.Vector3(0, 0, 4000);
+				let v = new THREE.Vector3(0, 0, this.data.radius);
 				v.applyAxisAngle(axisX, -star.dec);
 				v.applyAxisAngle(axisY, star.ra);
 
@@ -127,14 +128,14 @@ void main() {
 			clearInterval(this.intervalId);
 			let delta = this.data.speed * this.data.updateIntervalMs;
 			this.intervalId = setInterval(() => {
-				this.el.setAttribute("stars", { timeMs: this.data.timeMs + delta, realtime: false })
+				this.el.setAttribute("celestial-sphere", { timeMs: this.data.timeMs + delta, realtime: false })
 			}, this.data.updateIntervalMs);
 		}
 		const op = 365.242194 * 86400; // seconds
 		const oe = 84381.406 / 3600 * Math.PI / 180; // rad
 
 		let time = this.data.timeMs / 1000.0;
-		let d = (time - 79 * 86400) % op / op; // from spring equinox (79: Spring equinox in 1970)
+		let d = (time - 79.25 * 86400) % op / op; // from spring equinox (79: Spring equinox in 1970)
 		let deg = -360 * (d + time % 86400 / 86400) - this.data.lng;
 		this.el.object3D.rotation.set(THREE.Math.degToRad(90 - this.data.lat), THREE.Math.degToRad(deg), 0, 'XYZ');
 		if (this.constellations) {
@@ -158,11 +159,11 @@ void main() {
 			let axisY = new THREE.Vector3(0, 1, 0);
 			let axisZ = new THREE.Vector3(0, 0, 1);
 
-			let tt = time - 1174237200; // seconds from 2007/2/19
+			let tt = time - 1174240800; // seconds from 2007/3/19
 			let yy = tt / op;
 			let omvv = new THREE.Vector3(0, 0, 1).applyAxisAngle(axisY, aa * yy);
 			let omv = new THREE.Vector3(0, 1, 0).applyAxisAngle(omvv, om).applyAxisAngle(axisZ, oe);
-			let st = new THREE.Vector3(1, 0, 0).cross(omv).multiplyScalar(2000.0);
+			let st = new THREE.Vector3(1, 0, 0).cross(omv).multiplyScalar(this.data.radius * 0.98);
 			this.moon.position.copy(st.applyAxisAngle(omv, (tt % mop) / mop * 2 * Math.PI));
 		}
 	},
@@ -181,11 +182,11 @@ void main() {
 		let oev = new THREE.Vector3(0, 1, 0).applyAxisAngle(axisZ, oe);
 		d = d * 10;
 		for (let i = 0; i < d; i++) {
-			let v = new THREE.Vector3(0, 0, 1000).applyAxisAngle(axisY, Math.PI * 2 * i / d);
+			let v = new THREE.Vector3(0, 0, this.data.radius).applyAxisAngle(axisY, Math.PI * 2 * i / d);
 			geometry.vertices.push(v);
 			geometry.colors.push(new THREE.Color(0.8, 0, 0));
 
-			let ov = new THREE.Vector3(0, 0, 1000).applyAxisAngle(oev, Math.PI * 2 * i / d);
+			let ov = new THREE.Vector3(0, 0, this.data.radius).applyAxisAngle(oev, Math.PI * 2 * i / d);
 			geometry.vertices.push(ov);
 			geometry.colors.push(new THREE.Color(0.8, 0.8, 0.4));
 		}
@@ -196,7 +197,7 @@ void main() {
 		let yy = tt / op;
 		let omvv = new THREE.Vector3(0, 0, 1).applyAxisAngle(axisY, aa * yy);
 		let omv = new THREE.Vector3(0, 1, 0).applyAxisAngle(omvv, om).applyAxisAngle(axisZ, oe);
-		let st = new THREE.Vector3(1, 0, 0).cross(omv).multiplyScalar(2000.0);
+		let st = new THREE.Vector3(1, 0, 0).cross(omv).multiplyScalar(this.data.radius * 0.98);
 		for (let i = 0; i < 270; i++) {
 			geometry.vertices.push(st.clone().applyAxisAngle(omv, Math.PI * 2 * i / 270));
 			geometry.colors.push(new THREE.Color(0.6, 0.5, 0.1));
@@ -208,17 +209,19 @@ void main() {
 		this.gridPoints.visible = this.data.constellation;
 	},
 	_makeSun: function (d) {
-		let geometry = new THREE.SphereGeometry(18.5, 32, 32);
+		let r = this.data.radius * 0.99 * 2 * Math.PI * 1.06 / 360;
+		let geometry = new THREE.SphereGeometry(r, 32, 32);
 		let material = new THREE.MeshBasicMaterial({ color: 0xffffee, fog: false });
 		let sun = new THREE.Mesh(geometry, material);
-		sun.position.z = 2000;
+		sun.position.z = this.data.radius * 0.99;
 		let sunC = new THREE.Object3D();
 		sunC.add(sun);
 		this.el.object3D.add(sunC);
 		this.sun = sunC;
 	},
 	_makeMoon: function (d) {
-		let geometry = new THREE.SphereGeometry(19, 32, 32);
+		let r = this.data.radius * 0.99 * 2 * Math.PI * 1.03 / 360;
+		let geometry = new THREE.SphereGeometry(r, 32, 32);
 		let material = new THREE.MeshBasicMaterial({ color: 0x555544, fog: false });
 		let moon = new THREE.Mesh(geometry, material);
 		this.el.object3D.add(moon);
@@ -330,26 +333,26 @@ AFRAME.registerComponent('main-menu', {
 			document.querySelector('a-scene').exitVR();
 		});
 		this._getEl('constellations').addEventListener('click', (e) => {
-			var el = document.querySelector("[stars]");
-			el.setAttribute("stars", "constellation", !el.getAttribute("stars").constellation);
+			var el = document.querySelector("[celestial-sphere]");
+			el.setAttribute("celestial-sphere", "constellation", !el.getAttribute("celestial-sphere").constellation);
 		});
 		this._getEl('speed-x1').addEventListener('click', (e) => {
-			document.querySelector("[stars]").setAttribute("stars", "speed", 1.0);
+			document.querySelector("[celestial-sphere]").setAttribute("celestial-sphere", "speed", 1.0);
 		});
 		this._getEl('speed-x60').addEventListener('click', (e) => {
-			document.querySelector("[stars]").setAttribute("stars", "speed", 60.0);
+			document.querySelector("[celestial-sphere]").setAttribute("celestial-sphere", "speed", 60.0);
 		});
 		this._getEl('speed-x300').addEventListener('click', (e) => {
-			document.querySelector("[stars]").setAttribute("stars", "speed", 300.0);
+			document.querySelector("[celestial-sphere]").setAttribute("celestial-sphere", "speed", 300.0);
 		});
 		this._getEl('speed-x3600').addEventListener('click', (e) => {
-			document.querySelector("[stars]").setAttribute("stars", "speed", 3600.0);
+			document.querySelector("[celestial-sphere]").setAttribute("celestial-sphere", "speed", 3600.0);
 		});
 		this._getEl('time-now').addEventListener('click', (e) => {
-			document.querySelector("[stars]").setAttribute("stars", "timeMs", Date.now());
+			document.querySelector("[celestial-sphere]").setAttribute("celestial-sphere", "timeMs", Date.now());
 		});
 		this.timer = setInterval(() => {
-			let t = new Date(document.querySelector("[stars]").getAttribute("stars").timeMs);
+			let t = new Date(document.querySelector("[celestial-sphere]").getAttribute("celestial-sphere").timeMs);
 			let d2 = n => ("0" + n).substr(-2);
 			let timeStr = [t.getFullYear(), d2(t.getMonth() + 1), d2(t.getDate())].join("-") + " " +
 				[d2(t.getHours()), d2(t.getMinutes()), d2(t.getSeconds())].join(":");
@@ -397,13 +400,13 @@ AFRAME.registerComponent('config-dialog', {
 		this.lngEl = this._getEl('lng');
 		this.speedEl = this._getEl('speed');
 		this.timeEl = this._getEl('time');
-		this.targetEl = document.querySelector("[stars]");
+		this.targetEl = document.querySelector("[celestial-sphere]");
 
 		this._getEl('apply').addEventListener('click', (e) => {
 			this.el.setAttribute("visible", false);
 			let tt = this.timeEl.value.split(/[: /-]+/).map(a => a * 1);
 			let t = new Date(tt[0], tt[1] - 1, tt[2], tt[3] || 0, tt[4] || 0, tt[5] || 0);
-			this.targetEl.setAttribute("stars", {
+			this.targetEl.setAttribute("celestial-sphere", {
 				lat: this.latEl.value * 1.0, lng: this.lngEl.value * 1.0,
 				timeMs: t.getTime(), speed: this.speedEl.value * 1.0
 			});
@@ -416,10 +419,11 @@ AFRAME.registerComponent('config-dialog', {
 	remove: function () {
 	},
 	showDialog: function () {
-		this.latEl.value = this.targetEl.components.stars.data.lat;
-		this.lngEl.value = this.targetEl.components.stars.data.lng;
-		this.speedEl.value = this.targetEl.components.stars.data.speed;
-		let t = new Date(this.targetEl.components.stars.data.timeMs);
+		var attrs = this.targetEl.getAttribute("celestial-sphere");
+		this.latEl.value = attrs.lat;
+		this.lngEl.value = attrs.lng;
+		this.speedEl.value = attrs.speed;
+		let t = new Date(attrs.timeMs);
 		let d2 = n => ("0" + n).substr(-2);
 		this.timeEl.value = [t.getFullYear(), d2(t.getMonth() + 1), d2(t.getDate())].join("-") + " " +
 			[d2(t.getHours()), d2(t.getMinutes()), d2(t.getSeconds())].join(":");
