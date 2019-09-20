66e048266e0b7b24efba22e66cde80e0369d1c4f
Kousuke Kawahira <kawahira@binzume.net>
2019-09-20 23:48:01 +0900

    add svg files

---
+++ icons/gen.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set F=
+for %%f in (icon_cursor icon_lines icon_grid icon_solar icon_config) do (
+  msdfgen.exe -svg %%f.svg -o %%f.png -size 64 64 -scale 1
+  set F=!F! %%f.png
+)
+
+echo %F%
+go run pack.go -o icons.png %F%
+go run decode_msdf.go icons.png
