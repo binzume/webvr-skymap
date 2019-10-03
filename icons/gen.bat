e8eafa4c70e23a1abebf6fdad1032451a16ee26a
Kousuke Kawahira <kawahira@binzume.net>
2019-10-04 00:38:36 +0900

    add exit icon.

--- icons/gen.bat
+++ icons/gen.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 set F=
-for %%f in (icon_cursor icon_lines icon_grid icon_solar icon_config) do (
+for %%f in (icon_cursor icon_lines icon_grid icon_solar icon_config icon_exit) do (
   msdfgen.exe -svg %%f.svg -o %%f.png -size 64 64 -scale 1
   set F=!F! %%f.png
 )
